62359db5f8a709c302e4ddfbba3872e6f3a1ecd6
CharalambosIoannou <cioannou1997@gmail.com>
2023-04-14 14:04:02 +0300

    HMCA-13 Added boiler plate for streamlit

---
+++ streamlit/boilerplate/Homepage.py
@@ -0,0 +1,29 @@
+# Contents of ~/my_app/main_page.py
+import streamlit as st
+
+st.markdown("# Homepage")
+st.sidebar.markdown("# Homepage")
+
+
+# import pandas as pd
+
+# df = pd.DataFrame(
+#     [
+#         {"command": "st.selectbox", "rating": 4, "is_widget": True},
+#         {"command": "st.balloons", "rating": 5, "is_widget": False},
+#         {"command": "st.time_input", "rating": 3, "is_widget": True},
+#     ]
+# )
+
+# st.dataframe(df, use_container_width=True)
+
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# arr = np.random.normal(1, 1, size=100)
+# fig, ax = plt.subplots()
+# ax.hist(arr, bins=20)
+
+# st.pyplot(fig)
+
---
+++ streamlit/boilerplate/pages/Physical Attribute Analysis.py
@@ -0,0 +1,5 @@
+# Contents of ~/my_app/pages/page_2.py
+import streamlit as st
+
+st.markdown("# PAN")
+st.sidebar.markdown("# PAN")
